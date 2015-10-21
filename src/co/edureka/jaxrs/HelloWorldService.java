3a67373ac260c88ee8f96808850fd41133fa0f65
EdurekaRepository <lovleen.bhatia@edureka.co>
2015-10-21 18:02:06 +0530

    Conditional Get- ETag

--- src/co/edureka/jaxrs/HelloWorldService.java
+++ src/co/edureka/jaxrs/HelloWorldService.java
@@ -1,39 +1,47 @@
 package co.edureka.jaxrs;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.TimeZone;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.CacheControl;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.EntityTag;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
  
-@Path("/say")
+@Path("/et")
 public class HelloWorldService {
- 
+   
+	private static int count=1;
 	@GET
-	
-	public Response getMsg() throws ParseException {
+	@Produces("text/plain")
+	public Response getMsg(@Context Request request) throws ParseException {
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
-		Date current=new Date();
-		String date=sdf.format(current);
-		System.out.println(date);
+	    System.out.println("Got Request "+count);
+		count++;
+			
+		Date current=new Date();		
+		int minutes=current.getMinutes();		
 		
-		CacheControl cc=new CacheControl();
+	    CacheControl cc=new CacheControl();
 		cc.setMaxAge(60);
-		cc.setPrivate(true);
-		//cc.setNoStore(true);  It will not cache
 		
-		ResponseBuilder builder = Response.ok("Great :"+date.toString(), "text/plain");
+		EntityTag etag=new EntityTag(""+minutes);
+		
+		ResponseBuilder builder=request.evaluatePreconditions(etag);
+		if(builder !=null){
+			builder.cacheControl(cc);
+			return builder.build();
+		}		
+		
+		//Sending updated Response
+		builder=Response.ok("Time "+minutes,"text/plain");
 		builder.cacheControl(cc);
-		 return builder.build();
- 
-	}
- 
+		builder.tag(etag);
+		return builder.build(); 
+	} 
 }
