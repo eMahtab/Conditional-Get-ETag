e69cdbcbdf0c8937a03088af726ce2eb26fd93a0
EdurekaRepository <lovleen.bhatia@edureka.co>
2015-10-21 15:21:43 +0530

    HTTP 1.1 Cache-Control Header

--- src/co/edureka/jaxrs/HelloWorldService.java
+++ src/co/edureka/jaxrs/HelloWorldService.java
@@ -9,10 +9,11 @@ import java.util.TimeZone;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
  
-@Path("/hello")
+@Path("/say")
 public class HelloWorldService {
  
 	@GET
@@ -24,8 +25,13 @@ public class HelloWorldService {
 		String date=sdf.format(current);
 		System.out.println(date);
 		
+		CacheControl cc=new CacheControl();
+		cc.setMaxAge(60);
+		cc.setPrivate(true);
+		//cc.setNoStore(true);  It will not cache
+		
 		ResponseBuilder builder = Response.ok("Great :"+date.toString(), "text/plain");
-		builder.expires(new Date((current.getTime()+2*60*1000)));
+		builder.cacheControl(cc);
 		 return builder.build();
  
 	}
